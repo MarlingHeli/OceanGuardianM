c01a45108296cf0b69ad73cfcaac3b4499e3930d
MarlingHeli <mh20179@student.phs.school.nz>
2023-10-18 11:53:25 +1300

    Test: adding a background/surface

--- OceanGuardianM/OceanGuardianM.py
+++ OceanGuardianM/OceanGuardianM.py
@@ -7,6 +7,10 @@ pygame.init() #starts up pygame components
 #create display window
 screen = pygame.display.set_mode((1100, 750))
 pygame.display.set_caption("Ocean Guardian") #set name of display window
+#Set constant frame rate
+clock = pygame.time.Clock()
+
+test_surface = pygame.image.load("bg.png")
 
 #keep display window on screen forever
 while True:
@@ -15,7 +19,8 @@ while True:
         if event.type == pygame.QUIT: 
             pygame.quit()
             exit() #exit while loop
-    #draw all our elements
-    #update everything
+    screen.blit(test_surface, (0,0)) #display image
+    
     pygame.display.update() #update elements to show on display window
+    clock.tick(60) #limit while true loop to 60fps 
 
