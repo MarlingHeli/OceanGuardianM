d63b9544baad31f9bd94e97f782a0e35d21a0e7a
MarlingHeli <mh20179@student.phs.school.nz>
2023-10-18 14:20:31 +1300

    Test adding a player

--- OceanGuardianM/OceanGuardianM.py
+++ OceanGuardianM/OceanGuardianM.py
@@ -6,21 +6,44 @@ pygame.init() #starts up pygame components
 
 #create display window
 screen = pygame.display.set_mode((1100, 750))
-pygame.display.set_caption("Ocean Guardian") #set name of display window
+pygame.display.set_caption("Ocean Guardian M") #set name of display window
 #Set constant frame rate
 clock = pygame.time.Clock()
-test_font = pygame.font.Font(None, 50)
-text_surface = test_font.render("Hello", False, "White")
+text = pygame.font.Font(None, 50) #pick font for text.
+
+#starting co-ords
+x = 200
+y = 200
+
+#width of player
+width = 20
+height = 20
+
+#velocity of player
+velocity = 10
 
 #keep display window on screen forever
 while True:
+    pygame.time.delay(10) #delay (optional)
     #close display window if user quits
     for event in pygame.event.get(): #check for events constantly
         if event.type == pygame.QUIT: 
             pygame.quit()
             exit() #exit while loop
-            
-    screen.blit(text_surface,(500,300))
+    
+    #stores keys pressed
+    keys = pygame.key.get_pressed()
+    #if A or left arrow key is pressed
+    if keys[pygame.K_LEFT] and x > 0:
+        x -= velocity
+    if keys[pygame.K_RIGHT] and x < 500 - width:
+        x += velocity
+    if keys[pygame.K_UP] and y > 0:
+        y -= velocity
+    if keys[pygame.K_DOWN] and y < 500 - height:
+        y += velocity
+        
+    pygame.draw.rect(screen,(0,0,225), (x, y, width, height))
     
     pygame.display.update() #update elements to show on display window
     clock.tick(60) #limit while true loop to 60fps 
